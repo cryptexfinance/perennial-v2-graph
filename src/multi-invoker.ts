6a23fa6ed8698be3794b58b15a7e4f700a025cd9
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-12-20 12:05:18 -0500

    multiinvoker schema changes

--- src/multi-invoker.ts
+++ src/multi-invoker.ts
@@ -1,30 +1,54 @@
-import { Address, BigInt, ByteArray, Bytes, crypto } from '@graphprotocol/graph-ts'
+import { Address, BigInt, Bytes, crypto } from '@graphprotocol/graph-ts'
 import {
-  FeeCharged as FeeChargedEvent,
+  FeeCharged as FeeChargedEvent_Deprecated,
+  KeeperCall as KeeperCallEvent_Deprecated,
+  OrderExecuted as OrderExecutedEvent_Deprecated,
+  OrderPlaced as OrderPlacedEvent_Deprecated,
+} from '../generated/MultiInvoker_v2.0/MultiInvoker'
+import {
+  InterfaceFeeCharged as InterfaceFeeChargedEvent,
   Initialized as InitializedEvent,
   KeeperCall as KeeperCallEvent,
   KeeperFeeCharged as KeeperFeeChargedEvent,
-  OrderCancelled as OrderCancelledEvent,
   OrderExecuted as OrderExecutedEvent,
+  OrderCancelled as OrderCancelledEvent,
   OrderPlaced as OrderPlacedEvent,
 } from '../generated/MultiInvoker/MultiInvoker'
 import {
   MultiInvokerFeeCharged,
   Initialized,
+  MultiInvokerKeeperCall_Deprecated,
   MultiInvokerKeeperCall,
   MultiInvokerKeeperFeeCharged,
   MultiInvokerOrderCancelled,
   MultiInvokerOrderExecuted,
   MultiInvokerOrderPlaced,
 } from '../generated/schema'
+import { ZeroAddress } from './utils/constants'
 
-export function handleFeeCharged(event: FeeChargedEvent): void {
+export function handleFeeCharged_Deprecated(event: FeeChargedEvent_Deprecated): void {
   let entity = new MultiInvokerFeeCharged(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.account = event.params.account
+  entity.market = ZeroAddress // FeeCharged events do not have a market attached
   entity.to = event.params.to
   entity.amount = event.params.amount
+  entity.unwrap = true // FeeCharged events are always USDC
 
   entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInterfaceFeeCharged(event: InterfaceFeeChargedEvent): void {
+  let entity = new MultiInvokerFeeCharged(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  entity.account = event.params.account
+  entity.market = event.params.market
+  entity.to = event.params.fee.receiver
+  entity.amount = event.params.fee.amount
+  entity.unwrap = event.params.fee.unwrap
+
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
@@ -42,12 +66,28 @@ export function handleInitialized(event: InitializedEvent): void {
   entity.save()
 }
 
-export function handleKeeperCall(event: KeeperCallEvent): void {
-  let entity = new MultiInvokerKeeperCall(event.transaction.hash.concatI32(event.logIndex.toI32()))
+export function handleKeeperCall_Deprecated(event: KeeperCallEvent_Deprecated): void {
+  let entity = new MultiInvokerKeeperCall_Deprecated(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.sender = event.params.sender
   entity.gasUsed = event.params.gasUsed
   entity.multiplier = event.params.multiplier
   entity.buffer = event.params.buffer
+  entity.keeperFee = event.params.keeperFee
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleKeeperCall(event: KeeperCallEvent): void {
+  let entity = new MultiInvokerKeeperCall(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  entity.sender = event.params.sender
+  entity.applicableGas = event.params.applicableGas
+  entity.applicableValue = event.params.applicableValue
+  entity.baseFee = event.params.baseFee
+  entity.calldataFee = event.params.calldataFee
   entity.keeperFee = event.params.keeperFee
 
   entity.blockNumber = event.block.number
@@ -94,6 +134,21 @@ export function handleOrderCancelled(event: OrderCancelledEvent): void {
   }
 }
 
+export function handleOrderExecuted_Deprecated(event: OrderExecutedEvent_Deprecated): void {
+  return handleOrderExecuted(
+    new OrderExecutedEvent(
+      event.address,
+      event.logIndex,
+      event.transactionLogIndex,
+      event.logType,
+      event.block,
+      event.transaction,
+      event.parameters,
+      event.receipt,
+    ),
+  )
+}
+
 export function handleOrderExecuted(event: OrderExecutedEvent): void {
   const id = orderId(event.params.market, event.params.account, event.params.nonce)
   // Check if this is a noOp execution
@@ -103,7 +158,6 @@ export function handleOrderExecuted(event: OrderExecutedEvent): void {
   entity.account = event.params.account
   entity.market = event.params.market
   entity.nonce = event.params.nonce
-  entity.positionId = event.params.positionId
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -118,6 +172,30 @@ export function handleOrderExecuted(event: OrderExecutedEvent): void {
   }
 }
 
+export function handleOrderPlaced_Deprecated(event: OrderPlacedEvent_Deprecated): void {
+  let entity = new MultiInvokerOrderPlaced(orderId(event.params.market, event.params.account, event.params.nonce))
+  entity.account = event.params.account
+  entity.market = event.params.market
+  entity.nonce = event.params.nonce
+  entity.order_side = event.params.order.side
+  entity.order_comparison = event.params.order.comparison
+  entity.order_fee = event.params.order.fee
+  entity.order_price = event.params.order.price
+  entity.order_delta = event.params.order.delta
+  entity.order_interfaceFee_amount = BigInt.zero() // v2.0 orders did not have interface fees attached
+  entity.order_interfaceFee_receiver = ZeroAddress
+  entity.order_interfaceFee_unwrap = true
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.executed = false
+  entity.cancelled = false
+
+  entity.save()
+}
+
 export function handleOrderPlaced(event: OrderPlacedEvent): void {
   let entity = new MultiInvokerOrderPlaced(orderId(event.params.market, event.params.account, event.params.nonce))
   entity.account = event.params.account
@@ -128,6 +206,9 @@ export function handleOrderPlaced(event: OrderPlacedEvent): void {
   entity.order_fee = event.params.order.fee
   entity.order_price = event.params.order.price
   entity.order_delta = event.params.order.delta
+  entity.order_interfaceFee_amount = event.params.order.interfaceFee.amount
+  entity.order_interfaceFee_receiver = event.params.order.interfaceFee.receiver
+  entity.order_interfaceFee_unwrap = event.params.order.interfaceFee.unwrap
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
